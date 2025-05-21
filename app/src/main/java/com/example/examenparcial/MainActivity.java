0bac092509b2c42a7445c2a6f7b7321063fb4f98
Moisés Tafur Zegarra <mtafurz1011@gmail.com>
2025-05-21 18:46:07 -0500

    add login

--- app/src/main/java/com/example/examenparcial/MainActivity.java
+++ app/src/main/java/com/example/examenparcial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.examenparcial;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USUARIO_CORRECTO = "admin";
+    private static final String CONTRASENA_CORRECTA = "1234";
+    EditText etUsuario, etContrasena;
+    Button btnIngresar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etUsuario = findViewById(R.id.etUsuario);
+        etContrasena = findViewById(R.id.etContrasenia);
+        btnIngresar = findViewById(R.id.btnLogin);
+
+        btnIngresar.setOnClickListener(v -> {
+            String usuarioIngresado = etUsuario.getText().toString();
+            String contrasenaIngresada = etContrasena.getText().toString();
+
+            if (usuarioIngresado.equals(USUARIO_CORRECTO) && contrasenaIngresada.equals(CONTRASENA_CORRECTA)) {
+                Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+

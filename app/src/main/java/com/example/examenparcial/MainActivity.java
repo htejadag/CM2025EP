8b86e4c1548f81025acbdbd6d0d78b63b0bcd694
RonaldoSol7 <132590908+Ronaldo129@users.noreply.github.com>
2025-05-21 19:58:48 -0500

    eder

--- app/src/main/java/com/example/examenparcial/MainActivity.java
+++ app/src/main/java/com/example/examenparcial/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.examenparcial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+public final static String user="admin";
+    public final static String pass="admin";
+    public EditText txt_usuario;
+public EditText txt_password;
+public Button btn_inicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txt_password=findViewById(R.id.txtpassword);
+        txt_usuario=findViewById(R.id.txtusuario);
+        btn_inicio=findViewById(R.id.btninicio);
+        btn_inicio.setOnClickListener(v -> {
+            String usuarioIngresado = txt_usuario.getText().toString();
+            String passwordIngresado = txt_password.getText().toString();
+
+            if (usuarioIngresado.equals(user) && passwordIngresado.equals(pass)) {
+                Intent intent = new Intent(MainActivity.this, registropersona.class);
+                startActivity(intent);
+            } else if (!usuarioIngresado.equals(user) && !passwordIngresado.equals(pass)) {
+                Toast.makeText(MainActivity.this, "CREDENCIALES INCORRECTAS", Toast.LENGTH_SHORT).show();
+            } else if (!usuarioIngresado.equals(user)) {
+                Toast.makeText(MainActivity.this, "Usuario incorrecto", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Password incorrecto", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/examenparcial/registropersona.java
@@ -0,0 +1,86 @@
+package com.example.examenparcial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class registropersona extends AppCompatActivity {
+public EditText txt_nombre;
+    public EditText txt_correo;
+    public EditText txt_numero;
+    public RadioGroup sex_grupo;
+    public CheckBox chk_terminos;
+    public Button btn_validar;
+    public Button btn_retornar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registropersona);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        txt_nombre = findViewById(R.id.txtnombre);
+        txt_numero = findViewById(R.id.txtnumero);
+        txt_correo = findViewById(R.id.txtcorreo);
+        sex_grupo = findViewById(R.id.sexoGroup);
+        chk_terminos = findViewById(R.id.chkTerminos);
+        btn_retornar = findViewById((R.id.btnretornar));
+        btn_retornar.setOnClickListener(v -> {
+            Intent intent = new Intent(registropersona.this, MainActivity.class);
+            startActivity(intent);
+        });
+        btn_validar = findViewById((R.id.btnValidar));
+
+        btn_validar.setOnClickListener(v -> {
+            validarFormulario();;
+        });
+
+
+    }
+    private void validarFormulario(){
+        String nombre = txt_nombre.getText().toString();
+        String correo = txt_correo.getText().toString();
+        String numero = txt_numero.getText().toString();
+        if (nombre.isEmpty()) {
+            Toast.makeText(this, "Por favor ingrese su nombre", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if (correo.isEmpty() || !correo.contains("@")) {
+            Toast.makeText(this, "Por favor ingrese un correo válido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (numero.isEmpty() || numero.length() != 9) {
+            Toast.makeText(this, "Por favor ingrese un número válido (9 dígitos)", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int selectedSexoId = sex_grupo.getCheckedRadioButtonId();
+        if (selectedSexoId == -1) {
+            Toast.makeText(this, "Por favor seleccione su sexo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!chk_terminos.isChecked()) {
+            Toast.makeText(this, "Debe aceptar los términos y condiciones", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, "Formulario válido, puedes continuar", Toast.LENGTH_SHORT).show();
+
+    }
+
+}

29e6ffc29772af56500b2a194244002890bf1f3c
raphael arenales <raphaelarenalesv@gmail.com>
2025-05-21 19:45:21 -0500

    "Entrega de examen Parcial, Raphael Arenales"

---
+++ app/src/main/java/com/example/examenparcial/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.examenparcial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText usuario, contra;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usuario = findViewById(R.id.usuario);
+        contra = findViewById(R.id.contra);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuario.getText().toString().trim();
+                String pass = contra.getText().toString().trim();
+
+                if (user.equals("admin") && pass.equals("1234")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/examenparcial/MainActivity.java
+++ app/src/main/java/com/example/examenparcial/MainActivity.java
@@ -1,24 +1,92 @@
 package com.example.examenparcial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.*;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNombre, etCorreo, etCelular, etFecha;
+    RadioGroup rgGenero;
+    CheckBox checkTerminos;
+    Button btnValidar, btnRegresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Enlazar vistas del layout
+        etNombre = findViewById(R.id.etNombre);
+        etCorreo = findViewById(R.id.etCorreo);
+        etCelular = findViewById(R.id.etCelular);
+        etFecha = findViewById(R.id.etFecha);
+        rgGenero = findViewById(R.id.rgGenero);
+        checkTerminos = findViewById(R.id.checkTerminos);
+        btnValidar = findViewById(R.id.btnValidar);
+        btnRegresar = findViewById(R.id.btnRegresar);
+
+        btnValidar.setEnabled(false);
+
+        checkTerminos.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            btnValidar.setEnabled(isChecked);
         });
+
+
+        btnValidar.setOnClickListener(v -> {
+            if (!validarFormulario()) {
+                Toast.makeText(this, "Corrija los errores", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Datos correctos", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        btnRegresar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private boolean validarFormulario() {
+        boolean valido = true;
+
+        String nombre = etNombre.getText().toString().trim();
+        String correo = etCorreo.getText().toString().trim();
+        String celular = etCelular.getText().toString().trim();
+        String fecha = etFecha.getText().toString().trim();
+        int generoId = rgGenero.getCheckedRadioButtonId();
+
+        if (nombre.isEmpty() || nombre.length() > 15) {
+            etNombre.setError("Máximo puedes añadir 15 caracteres");
+            valido = false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            etCorreo.setError("Correo inválido");
+            valido = false;
+        }
+
+        if (celular.length() != 9 || !celular.matches("\\d{9}")) {
+            etCelular.setError("Debe contener exactamente 9 dígitos");
+            valido = false;
+        }
+
+        if (fecha.isEmpty()) {
+            etFecha.setError("Fecha obligatoria");
+            valido = false;
+        }
+
+        if (generoId == -1) {
+            Toast.makeText(this, "Seleccione un género", Toast.LENGTH_SHORT).show();
+            valido = false;
+        }
+
+        return valido;
     }
 }

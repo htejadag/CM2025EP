c9c76d26edfb8549d30b8c23559b56603291fe6b
jjminaya <jminayasantacruz@gmail.com>
2025-05-21 19:37:11 -0500

    formulario completo

--- app/src/main/java/com/example/examenparcial/MainActivity.java
+++ app/src/main/java/com/example/examenparcial/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.examenparcial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String user = "admin";
+    private static final String pass = "1234";
+    EditText etUsuario, etContrasena;
+    Button btnIngresar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        etUsuario = findViewById(R.id.etUsuario);
+        etContrasena = findViewById(R.id.etContrasenia);
+        btnIngresar = findViewById(R.id.btnLogin);
+
+        btnIngresar.setOnClickListener(v -> {
+            String usuarioIngresado = etUsuario.getText().toString();
+            String contrasenaIngresada = etContrasena.getText().toString();
+
+            if (usuarioIngresado.equals(user) && contrasenaIngresada.equals(pass)) {
+                Toast.makeText(this, "Inicò con exito", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, FormDatosP.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
